bb696a8120adad97116a4a3ade98b9ff09eff071
Christopher Ripley <clripley@gmail.com>
2020-07-06 22:26:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // add solution here
 function theBeatlesPlay() {
   var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
-  var instruments
+  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 }
