89b4ee077a2afd57f248bdebbc7f29d8df55e50b
Christopher Ripley <clripley@gmail.com>
2020-07-06 23:20:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,11 @@ i++;
 }
 return newArray;
 } 
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
+} 
