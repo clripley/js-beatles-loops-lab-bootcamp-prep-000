ba7aac31cbf4bf553453e59119745bf471be1600
Christopher Ripley <clripley@gmail.com>
2020-07-06 23:05:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,8 @@ function theBeatlesPlay(musicians, instruments){
   return newArray;
 } 
 function johnLennonfacts(facts) {
-  const facts = 
+  const facts = ["He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"]
 }
