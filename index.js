634fb5cf06eda34873e94a90e989cf0cd13c3d99
Christopher Ripley <clripley@gmail.com>
2020-07-06 23:06:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ function theBeatlesPlay(musicians, instruments){
   }
   return newArray;
 } 
-function johnLennonfacts(facts) {
-  const facts = ["He was the last Beatle to learn to drive",
-        "He was never a vegetarian",
-        "He was a choir boy and boy scout",
-        "He hated the sound of his own voice"]
+ const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+
+function johnLennonFacts(facts){
+var i=0;
+var newArray=[];
+while (facts.length > i){
+newArray.push(`${facts[i]}` + '!!!');
+i++;
 }
+return newArray;
+} 
